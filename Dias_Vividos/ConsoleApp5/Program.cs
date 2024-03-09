010830a010f98de6270e2a49ebb00ce4339a9d9b
Heradio <abiram.texis@uabc.edu.mx>
2024-03-09 01:02:26 -0800

    calcula dias vividos

---
+++ Dias_Vividos/ConsoleApp5/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            String nombre, apellidoP, apellidoM, telefono, direccion, sexo;
+            int A,diasV;
+            //0128246
+            //headio Abiram texis Virgen 
+            //pedir datos 
+            Console.ForegroundColor = ConsoleColor.Magenta;
+            Console.WriteLine("cual es tu nombre?");
+            nombre = Console.ReadLine();
+
+            Console.WriteLine("cul es tu apellido paterno?");
+            apellidoP= Console.ReadLine();
+
+            Console.WriteLine("cul es tu apellido Materno?");
+            apellidoM= Console.ReadLine();
+
+            Console.WriteLine("dame tu año de nacimiento :");
+            A= int.Parse(Console.ReadLine());
+            
+            DateTime fechaActual = DateTime.Now;
+            int edad = fechaActual.Year - A;
+            DateTime fechaNacimiento = new DateTime(A, 1, 1);
+            diasV = (int)(fechaActual - fechaNacimiento).TotalDays;
+
+            Console.WriteLine("dame tu numeo de telfono?");
+            telefono= Console.ReadLine();
+            Console.WriteLine("dame tu numeo de genero?");
+            sexo= Console.ReadLine();
+            Console.WriteLine("dame tu numeo de direccio?");
+            direccion= Console.ReadLine();
+            Console.ResetColor();
+            //imprimir 
+
+
+            Console.WriteLine("Tu nombre es : "+nombre);
+            Console.WriteLine("Tu Apellido pateno es : " + apellidoP);
+            Console.WriteLine("Tu Apelido matern es : " + apellidoM);
+            Console.WriteLine("Tu direccion es : " + direccion);
+            Console.WriteLine("Tu edad es : " + edad);
+            Console.WriteLine("Tu genero es : " + sexo);
+            Console.WriteLine("Tu telefono es : " + telefono);
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("Ha vivido aproximadamente " +diasV +" días.");
+            Console.ReadKey();
+        }
+    }
+}
