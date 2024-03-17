f13874552a0d130c2e71786d2862257e22eba1f6
HeradioTexis <abiram.texis@uabc.edu.mx>
2024-03-16 22:17:01 -0700

    Practicas JS

---
+++ Tabla_de_multiplicar/Tabla_de_multiplicar/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tabla_de_multiplicar
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Heradio Abiram Texis Virgen 
+            //01282456
+            int flag = 0, number;
+            Console.WriteLine("Bienvenido!!!");
+            Console.WriteLine("Tabla de multiplicar del 0 al 10");
+            Console.WriteLine("Ingresa un numero:");
+            number = int.Parse(Console.ReadLine());
+            while (flag <= 10)
+            {
+                Console.ForegroundColor = ConsoleColor.Cyan;
+                Console.WriteLine(number + " x " + flag + " = " + (number * flag));
+                flag++;
+            }
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ Viente_numeros/Viente_numeros/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Viente_numeros
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //heradio Abiram texis virgen 
+            //01282456
+            int cantidadNumeros = 20;
+            int[] numeros = new int[cantidadNumeros];
+
+            for (int i = 0; i < cantidadNumeros; i++)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.Write("Ingrese el número {0}: ", i + 1);
+                numeros[i] = int.Parse(Console.ReadLine());
+            }
+
+            
+            int mayor = numeros[0];
+            int menor = numeros[0];
+            for (int i = 1; i < cantidadNumeros; i++)
+            {
+                if (numeros[i] > mayor)
+                {
+                    mayor = numeros[i];
+                }
+
+                if (numeros[i] < menor)
+                {
+                    menor = numeros[i];
+                }
+            }
+
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("El número mayor es: {0}", mayor);
+           
+
+            Console.ReadLine();
+            Console.ReadKey();
+        }
+    }
+}
